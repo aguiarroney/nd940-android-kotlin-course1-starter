6991f0a67cea3049d14cbd6c50f30d665e5a97df
Roney Aguiar <raguiar@muxi.corp>
2021-03-05 17:05:12 -0300

    creating viewModel

---
+++ starter/app/src/main/java/com/udacity/shoestore/viewmodels/ShoeFragmentViewModel.kt
@@ -0,0 +1,19 @@
+package com.udacity.shoestore.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
+
+class ShoeFragmentViewModel: ViewModel() {
+
+    private var _shoeList: MutableLiveData<List<Shoe>> = MutableLiveData()
+    val shoeList: LiveData<List<Shoe>> = _shoeList
+
+    val teste: MutableLiveData<String> = MutableLiveData()
+
+    fun setTeste(s:String){
+        teste.value = s
+    }
+
+}
--- starter/app/src/main/java/com/udacity/shoestore/views/ShoesFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/views/ShoesFragment.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoesBinding
+import com.udacity.shoestore.viewmodels.ShoeFragmentViewModel
+import timber.log.Timber
 
 class ShoesFragment : Fragment() {
 
     private lateinit var binding: FragmentShoesBinding
+    private lateinit var viewModel: ShoeFragmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentShoesBinding.inflate(layoutInflater, container, false)
+
+        viewModel = ViewModelProvider(this).get(ShoeFragmentViewModel::class.java)
+
+        viewModel.setTeste("mudou o observer")
+
+        viewModel.teste.observe(viewLifecycleOwner, Observer {
+            Timber.i("Observou ${it}")
+        })
+
         return binding.root
     }
 }
