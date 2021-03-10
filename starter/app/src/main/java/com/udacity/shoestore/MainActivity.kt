c6bbccd4e7491f9c3fe6d909122e68098edd1226
Roney Aguiar <raguiar@muxi.corp>
2021-03-10 12:38:44 -0300

    navigateUp setup

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -7,7 +7,9 @@ import android.view.Menu
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
@@ -15,17 +17,24 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navHostFragment: NavHostFragment
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Timber.plant(Timber.DebugTree())
 
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.loginFragment,
+            R.id.shoesFragment
+        ))
+
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController)
+        NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navHostFragment.navController.navigateUp()
+        return navHostFragment.navController.navigateUp(appBarConfiguration)
     }
 }
